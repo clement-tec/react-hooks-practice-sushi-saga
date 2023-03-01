bc375e8b08708f0d07b4074ad55f6f3f06af2273
clement senou <clementsenou74@gmail.com>
2023-02-28 18:28:41 -0600

    complet delivrerables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+const API =` http://localhost:3001/sushis`;
 
 function App() {
+  const [renderSushis, setRenderSushis] = useState([]) 
+  const [offset, setOffset] = useState(0);
+  const [money, setMoney]= useState(200)
+  const DISPLAY_COUNT = 4
+  
+
+  useEffect(()=> {
+    fetch(API)
+    .then(response => response.json())
+    .then(json => setRenderSushis(json))
+  }, [])
+
+  function handleEatSushi(piece) {
+    const remainingMoney = money - piece.price
+    if(remainingMoney >=0) {
+    setMoney(remainingMoney)
+    setRenderSushis(renderSushis.map((sushi)=>sushi.id === piece.id?{...sushi, eaten: true} : sushi))
+    }
+} 
+
+  function handleMoreClick() {
+    setOffset((offset+DISPLAY_COUNT) %100)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer renderSushis={renderSushis.slice(offset, offset+DISPLAY_COUNT)}
+      handleMoreClick={handleMoreClick} handleEatSushi= {handleEatSushi}/>
+      <Table plates={ renderSushis.filter(sushi => sushi.eaten)}  money={money}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({renderSushis, handleMoreClick, handleEatSushi}) {
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderSushis.map((sushi)=> (<Sushi key ={sushi.id} sushi= {sushi} handleEatSushi={handleEatSushi} />
+      ))}
+      <MoreButton handleMoreClick={handleMoreClick} />
     </div>
   );
 }
